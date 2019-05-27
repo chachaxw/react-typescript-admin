fd2fa2ee133101b152efa189f45e058c8fbbdf5d
chacha <867571123@qq.com>
2019-05-27 10:07:49 +0800

    Update package.json

--- src/utils/errorHandle.ts
+++ src/utils/errorHandle.ts
@@ -5,34 +5,44 @@ import { codeMessage } from './constant';
 
 export const errorHandle = (error: any, dispatch: any) => {
   const { response } = error;
-  const { status, data } = response;
-  const errorText = codeMessage[status] || data.error;
 
-  if (status === 401) {
+  if (response) {
+    const { status, data } = response;
+    const errorText = codeMessage[status] || data.error;
+
+    if (status === 401) {
+      notification.error({
+        message: errorText,
+      });
+      dispatch({
+        type: 'global/deauthorize',
+        payload: { isAuthenticated: false, permissions: [] },
+      });
+      return;
+    }
+
     notification.error({
-      message: errorText,
+      message: `请求错误 ${status}`,
+      description: errorText,
     });
-    // TODO: need to add redirect to login page
-    // dispatch({type: 'global/logout'});
-    return;
-  }
 
-  notification.error({
-    message: `请求错误 ${status}`,
-    description: errorText,
-  });
+    if (status === 403) {
+      dispatch(routerRedux.push('/app/exception/403'));
+      return;
+    }
 
-  if (status === 403) {
-    dispatch(routerRedux.push('/app/exception/403'));
-    return;
-  }
+    if (status <= 504 && status >= 500) {
+      dispatch(routerRedux.push('/app/exception/500'));
+      return;
+    }
 
-  if (status <= 504 && status >= 500) {
-    dispatch(routerRedux.push('/app/exception/500'));
-    return;
-  }
-
-  if (status >= 404 && status < 422) {
-    dispatch(routerRedux.push('/app/exception/404'));
+    if (status >= 404 && status < 422) {
+      dispatch(routerRedux.push('/app/exception/404'));
+    }
+  } else {
+    notification.error({
+      message: `请求错误`,
+      description: '网络不佳！',
+    });
   }
 };
